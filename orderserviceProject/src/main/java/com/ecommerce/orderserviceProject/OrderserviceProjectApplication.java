d1fed41a205646196fc1acbba49969542aabffc5
Sandhyarani Kairam <sandhyaranikairam@Sandhyaranis-MacBook-Air.local>
2021-02-15 20:56:39 -0700

    initial project structure

---
+++ orderserviceProject/src/main/java/com/ecommerce/orderserviceProject/OrderserviceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.orderserviceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserviceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderserviceProjectApplication.class, args);
+	}
+
+}
---
+++ orderserviceProject/src/test/java/com/ecommerce/orderserviceProject/OrderserviceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.orderserviceProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
